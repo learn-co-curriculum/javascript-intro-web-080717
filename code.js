4061a23148f10e3d7229cb7762b84e754d619a08
alexgriff <alexalexgriffith@gmail.com>
2017-09-18 15:10:32 -0400

    post functions lecture

---
+++ code.js
@@ -0,0 +1,39 @@
+
+
+// write two functions
+// write these out using an anonynmos functions
+// and then with a named fucntion
+
+// use map
+
+// [{potato: 8}, {lettuce: 9}, {beet: 56}]
+// [8, 9, 56]
+
+
+// translate this ruby into js
+
+// arr.select do |num|
+//  num.odd?
+// end
+
+
+
+//
+// F = mass * acceleration
+// 9.8
+
+// forceOnMars(mass)
+// forceOnPlanetEarth(mass)
+
+
+// define a function called first
+// which takes a number (n) as argument
+// this should retutn a function that returns
+// the first n numbers of an array
+
+// arr = ['max', 'rachel', 'alex', 'johann', 'esmery']
+// first(3)(arr)
+// ['max', 'rachel', 'alex']
+
+//first(1)(arr)
+// ['max']
---
+++ ruby.rb
@@ -0,0 +1,15 @@
+def add(a, b)
+  return a + b
+end
+
+
+def your_favorite_programming_language
+  return 'javascript'
+end
+
+
+arr = (1..10).to_a
+
+arr.map do |num|
+  num * num
+end
